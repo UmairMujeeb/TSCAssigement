1b841172ab3589bb5cdc4512999cc21274cabd10
UmairMujeeb <160092258+UmairMujeeb@users.noreply.github.com>
2024-02-28 13:49:20 +0500

    1st Assigement 1 to 8
    Q/A 1 To 8

---
+++ Assigement.js
@@ -0,0 +1,49 @@
+"use strict";
+//-----2-----
+//let Name1: String; //variable declaration
+//Name1 = "Umair Mujeeb"; //value assigement
+//console.log(`Hello ${Name1}, would you like to learn some Python today?`);
+//-------3-------
+//let PersonName: String = "Umair Mujeeb";
+//console.log(PersonName.toLowerCase()); // Output: umair mujeeb
+//console.log(PersonName.toUpperCase()); // Output: UMAIR MUJEEB
+//let titlecasename: string = PersonName.toLowerCase().replace(/\b\w/g, (letter) => letter.toUpperCase());
+//console.log(titlecasename); // Output: Umair Mujeeb
+//-------4------
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+//let quote: string = "A person who never made a mistake never tried anything new.";
+//let author: string = "Albert Einstein";
+//console.log(`${author} once said, "${quote}"`);
+//--------5-------
+//let quote1: string = "A person who never made a mistake never tried anything new.";
+//let famous_person: string = "Albert Einstein";
+//let message: string =`${famous_person} once said, "${quote1}"`;
+//console.log(message);
+//--------6---------
+//let namewithwhitespace: string = "\t      Umair     \n";
+//console.log("Name With White Space: ");
+//console.log(namewithwhitespace);
+//let strippedname: string = namewithwhitespace.trim();
+//console.log("\nstripped name:  ");
+//console.log(strippedname);
+//------7----
+//Addition
+//let AdditionResult: number = 5 + 3;
+//console.log("Addition: " + AdditionResult);
+//Multiplication
+//let MultiplicationResult: number = 4 * 2;
+//console.log("Multiplication: " + MultiplicationResult);
+//Substraction
+//let SubstractionResult: number = 10 - 2;
+//console.log("Subtraction: " + SubstractionResult);
+//Division
+//let DivisionResult: number = 16 / 2;
+//console.log("Division: " + DivisionResult);
+console.log(5 + 3);
+//console.log(10 - 2);
+//console.log(4 * 2);
+//console.log(16 / 2);
+//-----------8-------------
+let Favourite_Number = 5;
+console.log("My Favourite Number Is: " + Favourite_Number);
+console.log(`My Favourite Number Is: ${Favourite_Number}`);
