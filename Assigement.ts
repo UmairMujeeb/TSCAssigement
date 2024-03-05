867c1a656207c6a8042dba3d83cf5b45cfbe729f
UmairMujeeb <160092258+UmairMujeeb@users.noreply.github.com>
2024-03-05 12:03:26 +0500

    Q/A 1 To 15

---
+++ Assigement.ts
@@ -0,0 +1,120 @@
+//-----2-----
+
+//let Name1: String; //variable declaration
+//Name1 = "Umair Mujeeb"; //value assigement
+//console.log(`Hello ${Name1}, would you like to learn some Python today?`);
+//-------3-------
+//let PersonName: String = "Umair Mujeeb";
+//console.log(PersonName.toLowerCase()); // Output: umair mujeeb
+//console.log(PersonName.toUpperCase()); // Output: UMAIR MUJEEB
+//let titlecasename: string = PersonName.toLowerCase().replace(/\b\w/g, (letter) => letter.toUpperCase());
+//console.log(titlecasename); // Output: Umair Mujeeb
+
+//-------4------
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+//let quote: string = "A person who never made a mistake never tried anything new.";
+//let author: string = "Albert Einstein";
+//console.log(`${author} once said, "${quote}"`);
+
+//--------5-------
+
+//let quote1: string = "A person who never made a mistake never tried anything new.";
+//let famous_person: string = "Albert Einstein";
+//let message: string =`${famous_person} once said, "${quote1}"`;
+//console.log(message);
+
+//--------6---------
+
+//let namewithwhitespace: string = "\t      Umair     \n";
+//console.log("Name With White Space: ");
+//console.log(namewithwhitespace);
+//let strippedname: string = namewithwhitespace.trim();
+//console.log("\nstripped name:  ");
+//console.log(strippedname);
+
+//------7----
+
+//Addition
+//let AdditionResult: number = 5 + 3;
+//console.log("Addition: " + AdditionResult);
+
+//Multiplication
+//let MultiplicationResult: number = 4 * 2;
+//console.log("Multiplication: " + MultiplicationResult);
+
+//Substraction
+//let SubstractionResult: number = 10 - 2;
+//console.log("Subtraction: " + SubstractionResult);
+
+//Division
+//let DivisionResult: number = 16 / 2;
+//console.log("Division: " + DivisionResult);
+
+//------8-----------
+
+//console.log(5+3);
+//console.log(10 - 2);
+//console.log(4 * 2);
+//console.log(16 / 2);
+
+//-----------9-------------
+
+//let Favourite_Number: number = 5;
+//console.log("My Favourite Number Is: " + Favourite_Number);
+//console.log(`My Favourite Number Is: ${Favourite_Number}`);
+
+//--------11------
+//let Name2: String = "Umair";
+//console.log(Name2);
+//let Names: string[] = ["Akram", "Aslam", "Akmal", "Ahmed", "Azher",];
+//console.log(Names);
+//console.log(Names[1]);
+//for(let i=0; i<5; i++)
+//{
+//    console.log(i);
+//    console.log(Names[i]);
+//}
+
+//------12------
+
+//const Names: string[] = ["Akram", "Aslam", "Akmal", "Ahmed", "Azher"];
+//for (let i = 0; i < Names.length; i++) {
+//    console.log(`Hello, ${Names[i]} I Hope you are doing well,`);
+//}
+
+//------13-------
+
+//const Transportation: string[] = ["Bicycle", "Motorcycle", "Scooter", "Car", "Boat"];
+//for (let i = 0; i < Transportation.length; i++) {
+//    console.log(`I Would like to own a ${Transportation[i]},`);
+//}
+
+//------14------
+
+//const guestlist: string[] = ["Umair", "Mujeeb", "Agwan"];
+//for (let i=0; i < guestlist.length; i++) {
+//    console.log(`Dear ${guestlist[i]}, You are cordially invited to dinner. We would be honored to have your presence.`);
+//}
+
+//-----15------
+
+//const guestlist: string[] = ["Umair", "Mujeeb", "Agwan"];
+//const Guestwhocantmakeit: string = guestlist[2];
+//console.log(`${Guestwhocantmakeit} Can't Make It To The Dinner.`);
+//const newguest: string = "UMA";
+//guestlist[2] = newguest;
+//for (let i=0; i < guestlist.length; i++) {
+//    console.log(`Dear ${guestlist[i]}, You are cordially invited to dinner. We would be honored to have your presence.`);
+//}
+
+//--------16--------
+
+//const guestlist: string[] = ["Umair", "Mujeeb", "Agwan", "fahad", "Hassan", "Hussain",];
+//const Guestwhocantmakeit: string = guestlist[2];
+//console.log(`${Guestwhocantmakeit} Can't Make It To The Dinner.`);
+//const newguest: string = "UMA";
+//guestlist[2] = newguest;
+//for (let i=0; i < guestlist.length; i++) {
+//    console.log(`Dear ${guestlist[i]}, You are cordially invited to dinner. We would be honored to have your presence.`);
+//} 
